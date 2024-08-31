84469947d45887f05159bf3a8de22837d2be8cc3
MauricioAraujo1 <araujomauricio2425@gmail.com>
2024-08-30 20:34:10 -0400

    feat: implementation of measureModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MeasureModule } from './measure.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MeasureModule, {
+    logger: false,
+  });
   await app.listen(3000);
 }
 bootstrap();
